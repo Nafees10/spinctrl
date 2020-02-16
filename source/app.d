a125940eae89d13428652b09d79bb67f3cf17ca6
Nafees10 <nafeeshassan16@gmail.com>
2020-02-16 21:38:46 +0500

    Added command history

--- source/app.d
+++ source/app.d
@@ -8,9 +8,14 @@ import qui.qui;
 import qui.widgets;
 
 import utils.misc;
+import utils.lists;
+
+import ctrl;
 
 /// default directory where images are read from
 const string DEFAULT_IMG_DIR = "~/.local/spinctrl/";
+/// path of command history file
+const string COMMAND_HISTORY_PATH = "~/.local/spinctrl/.history";
 
 /// safe RPM range, lower bound (inclusive)
 const uint[2] RANGE_RPM = [1000,2000];
@@ -79,6 +84,8 @@ private:
 	TextLabelWidget _labelRpm, _labelToggleTime, _labelDegLost, _labelFps; /// display RPM, toggleTime, degrees per update, FPS
 	LogWidget _log; /// log of all input commands, and status
 	ProgressbarWidget _progBar; /// display how much of a playing animation has been played
+	QLayout _inputLayout; /// holds widgets for getting input
+	TextLabelWidget _labelInput; /// to show where to input
 	EditLineWidget _commandInput; /// used to input commands
 // other vars
 	string _dir;
@@ -87,10 +94,42 @@ private:
 	
 	uinteger _rpm, _fps, _toggleTime, _degLost; /// stores last read values of  rpm, and toggleTime. _fps and _degLost are calculated
 	uinteger _animationFramesPlayed, _animationFramesTotal; /// number of frams of an animation that have been played/total
+	/// stores commands previously executed
+	List!string _commandHistory;
+	/// what index was previously selected from _commandHistory
+	uinteger _commandHistoryIndex;
 protected:
+	/// interpretes commands, & gets them executed
+	void execCommand(string command){
+
+	}
 	/// keyboard for _commandInput
 	void commandKeyboard(QWidget widget, KeyboardEvent key){
-		// TODO: write this thing
+		static string currentEntry = ""; /// stores what was entered before the user started using arrowUp/Down to select from history
+		if (key.key == KeyboardEvent.Key.UpArrow && _commandHistoryIndex >= 0){
+			if (_commandHistoryIndex >= _commandHistory.length){
+				currentEntry = _commandInput.text;
+				_commandHistoryIndex = _commandHistory.length;
+			}
+			if (_commandHistoryIndex > 0)
+				_commandHistoryIndex --;
+			if (_commandHistoryIndex < _commandHistory.length)
+				_commandInput.text = _commandHistory.read(_commandHistoryIndex);
+		}else if (key.key == KeyboardEvent.Key.DownArrow){
+			if (_commandHistoryIndex < _commandHistory.length){
+				_commandHistoryIndex ++;
+				if (_commandHistoryIndex < _commandHistory.length)
+					_commandInput.text = _commandHistory.read(_commandHistoryIndex);
+				else
+					_commandInput.text = currentEntry;
+			}
+		}else if (key.key == '\n' && _commandInput.text.length){
+			_commandHistoryIndex = uinteger.max;
+			_commandHistory.append(_commandInput.text);
+			_log.add("> "~_commandInput.text);
+			execCommand(_commandInput.text);
+			_commandInput.text = "";
+		}
 	}
 	/// timer event. Just using one timer event to update all widgets, avoiding loosing time in multiple function calls
 	void timer(QWidget widget){
@@ -133,6 +172,17 @@ public:
 	this(string dir, string serialPath){
 		_dir = dir;
 		_serialPath = serialPath;
+		_commandHistory = new List!string;
+		// try opening it
+		if (exists(expandTilde(COMMAND_HISTORY_PATH))){
+			const fileContents = fileToArray(expandTilde(COMMAND_HISTORY_PATH));
+			_commandHistory.setFreeSpace(fileContents.length);
+			foreach(line; fileContents){
+				if (line != "")
+					_commandHistory.append(line);
+			}
+		}
+		_commandHistoryIndex = _commandHistoryIndex.max;
 		// TODO: open the serial file
 
 		// set up the UI
@@ -144,16 +194,18 @@ public:
 		_labelDegLost = new TextLabelWidget();
 		_log = new LogWidget();
 		_progBar = new ProgressbarWidget(1,1);
+		_inputLayout = new QLayout(QLayout.Type.Horizontal);
+		_labelInput = new TextLabelWidget(">");
 		_commandInput = new EditLineWidget();
-		version(masterhacker){
-			// make all text in log and input green, so people thing we're "hAcKiNg"
-			_log.textColor = ALT_FG;
-			_commandInput.textColor = ALT_FG;
-			// </masterhacker>
-		}
 		// arrange the elements
 		_infoLayout.addWidget([_labelToggleTime, _labelRpm, _labelDegLost, _labelFps]);
-		_term.addWidget([_infoLayout, _log, _progBar, _commandInput]);
+		_inputLayout.addWidget([_labelInput, _commandInput]);
+		_term.addWidget([_infoLayout, _log, _progBar, _inputLayout]);
+		// set the input getters
+		_inputLayout.size.maxHeight = 1;
+		_labelInput.textColor = ALT_FG;
+		_labelInput.size.maxWidth = 1;
+		_commandInput.textColor = ALT_FG;
 		// set progress bar
 		_progBar.backgroundColor = DEFAULT_BG;
 		_progBar.barColor = ALT_FG;
@@ -161,9 +213,11 @@ public:
 		_progBar.size.maxHeight = 1;
 		// register all widgets
 		_term.registerWidget([_labelToggleTime, _labelRpm, _labelDegLost, _labelFps, _infoLayout, _log,
-			 _progBar, _commandInput]);
+			 _progBar, _inputLayout, _labelInput, _commandInput]);
 		// tie timer event to layout
 		_infoLayout.onTimerEvent = &timer;
+		// and the keyboard to _commandInput
+		_commandInput.onKeyboardEvent = &commandKeyboard;
 	}
 	/// destructor
 	~this(){
@@ -174,13 +228,17 @@ public:
 		.destroy(_labelDegLost);
 		.destroy(_labelFps);
 		.destroy(_log);
-		.destroy(_commandInput);
 		.destroy(_infoLayout);
+		.destroy(_labelInput);
+		.destroy(_inputLayout);
+		.destroy(_commandInput);
 		// now end the terminal itself
 		.destroy(_term);
 	}
 	/// runs the whole thing
 	void run(){
 		_term.run;
+		// save the command history file
+		_commandHistory.toArray().arrayToFile(expandTilde(COMMAND_HISTORY_PATH));
 	}
 }
