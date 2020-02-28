d1cc6fd4fc63aeafae2b27a58ba763f3db2338b7
Nafees10 <nafeeshassan16@gmail.com>
2020-02-28 15:35:45 +0500

    Improvement to RawFrame functions

--- source/spinctrl/img/img.d
+++ source/spinctrl/img/img.d
@@ -12,43 +12,43 @@ const ubyte GROUP_LEDS_COUNT = 4;
 /// `n` is the number of groups of LEDs available
 /// `sectors` is the number of sectors in a frame
 package struct RawFrame(ubyte n = 5, ubyte sectors = 72){
-	/// possible colors of LEDs
-	enum Color : ubyte{
-		Green, /// .
-		Blue, /// .
+	/// Returns: number of sectors
+	@property ubyte sectorCount(){
+		return sectors;
 	}
-	/// stores the raw image
+	/// Returns: number of groups of LEDs
+	@property ubyte groupCount(){
+		return n;
+	}
+	/// possible status of LEDs
+	enum Color : ubyte{
+		Off = 0B00000000, /// .
+		Green = 0B00000001, /// .
+		Blue = 0B00000010, /// .
+		Cyan = 0B00000011, /// .
+	}
+	/// stores the raw image, this directly represents the stream generated at end
 	ubyte[n][sectors] _imgData;
-	/// postblit
+	/// postblit, makes sure all _imgData are separate
 	this(this){
 		_imgData = _imgData.dup;
 	}
-	/// Returns: status of both color LEDs (true = on, false = off) at a sector.
-	/// Green are even indexes (including zero), blue are odd
-	bool[n*GROUP_LEDS_COUNT*2] readSector(ubyte sector){
-		bool[n * GROUP_LEDS_COUNT*2] r;
-		const ubyte[n] groups = _imgData[sector];
-		foreach(index, group; groups){
-			const ubyte index8 = index * 8;
-			foreach(i; 0 .. 8){
-				r[index8+(7 - i)] = (group >> i) % 2 ? true : false;
-			}
-		}
-		return r;
-	}
-	/// Returns: status of 1 colored LEDs (true = on, false = off) at a sector
-	bool[n*GROUP_LEDS_COUNT] readSector(ubyte sector, Color col){
-		/// offset due to color choice
-		const ubyte colorOffset = col == Color.Green ? 0 : 1;
-		bool[n * GROUP_LEDS_COUNT] r;
+	/// Returns: status of LEDs at a sector.
+	Color[n*GROUP_LEDS_COUNT] readSector(ubyte sector){
+		Color[n * GROUP_LEDS_COUNT] r;
 		const ubyte[n] groups = _imgData[sector];
 		foreach(index, group; groups){
 			const ubyte index4 = index * 4;
 			foreach(i; 0 .. 4){
-				r[index4+7 - ((i*2)+colorOffset)] = (group >> (i*2)+colorOffset) % 2 ? true : false;
+				r[index4+(3 - i)] = cast(Color)((group >>> (i*2)) % 3);
 			}
 		}
 		return r;
 	}
-
+	/// Writes to a sector
+	/// 
+	/// Returns: true on success, false on fail (i.e sector invalid)
+	bool writeSector(ubyte sector, Color[n*GROUP_LEDS_COUNT] ledStatus){
+		// TODO
+	}
 }
