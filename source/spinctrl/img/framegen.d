64c5628fb1bcb4be90c2d59c25063a7d32438685
Nafees10 <nafeeshassan16@gmail.com>
2020-03-06 01:23:27 +0500

    Implemented TrigCalc

--- source/spinctrl/img/framegen.d
+++ source/spinctrl/img/framegen.d
@@ -8,27 +8,71 @@ import utils.misc;
 import arsd.png;
 import arsd.color;
 
-// NOTE: CPixel or cpixel or cpix refers to a "pixel" on the circular display
+import std.math : sin, cos, PI, abs;
+
+public alias Color = spinctrl.img.imgdef.Color;
 
 /// To generate RawFrames from PNG images or draw directly to them
 public class FrameMake{
-
+private:
+	TrigCalc _calcu; /// used to find position of pixels
+	ubyte _sectors; /// number of sectors
+public:
+	/// constructor
+	this(ubyte sectors){
+		_sectors = sectors;
+		_calcu = new TrigCalc(_sectors);
+	}
+	~this(){
+		.destroy(_calcu);
+	}
 }
 
 /// Used by FrameMake to do trigonometric calculations
 private class TrigCalc{
 private:
-	/// stores x position of cpixels. Read as: _xpos[(sector*GROUP_LEDS_COUNT*GROUP_COUNT) + (cpix-GROUP_LEDS_COUNT*GROUPCOUNT)]
-	ubyte[] _xpos;
-	/// stores y position of cpixels. Read as: _ypos[(sector*GROUP_LEDS_COUNT*GROUP_COUNT) + (cpix-GROUP_LEDS_COUNT*GROUPCOUNT)]
-	ubyte[] _ypos;
+	/// stores led number for a [x, y]. Read as: [(y*width) + x]
+	ubyte[] _leds;
+	/// stores sector number for a [x, y]. Read as: [(y*width) + x]
+	ubyte[] _sectors;
 
 	/// stores number of sectors
-	ubyte _sectors;
+	ubyte _sectorsCount;
 
 	/// calculates _xpos and _ypos
 	void calculate(){
+		static const ubyte[2][4] origins = [
+			[GROUP_LEDS_COUNT*GROUP_COUNT,GROUP_LEDS_COUNT*GROUP_COUNT],
+			[(GROUP_LEDS_COUNT*GROUP_COUNT)-1,GROUP_LEDS_COUNT*GROUP_COUNT],
+			[(GROUP_LEDS_COUNT*GROUP_COUNT)-1,(GROUP_LEDS_COUNT*GROUP_COUNT)+1],
+			[(GROUP_LEDS_COUNT*GROUP_COUNT)+1,(GROUP_LEDS_COUNT*GROUP_COUNT)+1],
+		];
+		foreach(sector; 0 .. _sectorsCount){
+			uint x, y;
+			foreach(led; 0 .. GROUP_LEDS_COUNT * GROUP_COUNT){
+				immutable float abAngle = (360 / _sectorsCount) * sector;
+				immutable ubyte quadrant = abAngle <= 90 ? 1 : abAngle <= 180 ? 2 : abAngle <= 270 ? 3 : 4;
+				immutable float xDist = led * abs(cos(abAngle * (PI / 180)));
+				immutable float yDist = led * abs(sin(abAngle * (PI / 180)));
+				if (quadrant == 1){
+					x = cast(uint)(origins[1][0] + xDist);
+					y = cast(uint)(origins[1][1] - yDist);
+				}else if (quadrant == 2){
+					x = cast(uint)(origins[1][0] - xDist);
+					y = cast(uint)(origins[1][1] - yDist);
+				}else if (quadrant == 3){
+					x = cast(uint)(origins[1][0] - xDist);
+					y = cast(uint)(origins[1][1] + yDist);
+				}else if (quadrant == 4){
+					x = cast(uint)(origins[1][0] + xDist);
+					y = cast(uint)(origins[1][1] + yDist);
+				}
 
+				immutable uint index = (y*GROUP_COUNT*GROUP_LEDS_COUNT) + x;
+				_leds[index] = cast(ubyte)led;
+				_sectors[index] = cast(ubyte)sector;
+			}
+		}
 	}
 public:
 	/// constructor
@@ -37,16 +81,12 @@ public:
 	/// 
 	/// `sectorsCount` is number of sectors
 	this(ubyte sectorsCount){
-		_sectors = sectorsCount;
+		_sectorsCount = sectorsCount;
 		calculate();
 	}
-	/// Returns: [x, y] coordinates of a CPixel at a sector
-	ubyte[2] getCPixelPosition(ubyte cPixel, ubyte sector){
-		immutable uint index = (sector*GROUP_LEDS_COUNT*GROUP_COUNT) + (cPixel-GROUP_LEDS_COUNT*GROUP_COUNT);
-		return [_xpos[index], _ypos[index]];
-	}
-	/// Returns: [sector, cPixel] coordinates of a x, y pixel
+	/// Returns: [sector, led] coordinates of a x, y pixel
 	ubyte[2] getPixelPosition(ubyte x, ubyte y){
-		
+		immutable uint index = (y*GROUP_COUNT*GROUP_LEDS_COUNT) + x;
+		return [_sectors[index], _leds[index]];
 	}
 }
