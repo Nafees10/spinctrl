e67ce08d747778ec076591d39e478854eae86025
Nafees10 <nafeeshassan16@gmail.com>
2020-03-06 10:13:46 +0500

    Bug fixes

--- source/spinctrl/img/framegen.d
+++ source/spinctrl/img/framegen.d
@@ -31,31 +31,33 @@ int[2] getLedAngle(ubyte x, ubyte y){
 	/// center lines
 	immutable ubyte center = GROUP_COUNT*GROUP_LEDS_COUNT; // 20
 	/// stores quadrant
-	immutable ubyte quadrant = x < center ? (y < center ? 2 : 3) : (y < center ? 1 : 4);
+	immutable ubyte quadrant = x < center ? (y <= center ? 2 : 3) : (y <= center ? 1 : 4);
 	// x, y, and hypotenuse distance from origin
 	immutable uint xDist = x == center ? 0 : abs(center - x);
 	immutable uint yDist = y == center ? 0 : abs(center - y);
 	immutable uint led = cast(immutable uint)round(sqrt(cast(float)(pow(xDist, 2) + pow(yDist, 2))));
-	if (xDist + yDist == 0 || led > center)
+	if (xDist + yDist == 0 || led > center || led == 0)
 		return [-1, -1];
 	// now calculate angle
-	immutable float baseAngle = abs(atan2(cast(float)xDist, cast(float)yDist)) * (180 / PI);
+	immutable float baseAngle = abs(atan2(cast(float)yDist, cast(float)xDist)) * (180 / PI);
 	immutable uint angle = cast(uint)(quadrant == 1 ? baseAngle : quadrant == 2 ? 180 - baseAngle : 
 		quadrant == 3 ? 180 + baseAngle : 360 - baseAngle);
-	return [angle, led];
+	return [angle, led-1];
 }
 /// 
 unittest{
-	TrueColorImage img = new TrueColorImage(41, 41);
-	foreach (ubyte x; 0 .. 41){
-		foreach (ubyte y; 0 .. 41){
-			int[2] pos = getLedAngle(x, y);
-			if (pos[0] == -1 && pos[1] == -1){
-				img.setPixel(x, y, arsd.color.Color.red);
-			}else{
-				img.setPixel(x, y, arsd.color.Color.green);
+	debug{
+		TrueColorImage img = new TrueColorImage(41, 41);
+		foreach (ubyte x; 0 .. 41){
+			foreach (ubyte y; 0 .. 41){
+				int[2] pos = getLedAngle(x, y);
+				if (pos[0] == -1 && pos[1] == -1){
+					img.setPixel(x, y, arsd.color.Color.red);
+				}else{
+					img.setPixel(x, y, arsd.color.Color.green);
+				}
 			}
 		}
+		writePng("spot.png", img);
 	}
-	writePng("spot.png", img);
 }
