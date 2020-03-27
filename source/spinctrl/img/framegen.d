2e55c1009e93f30c7c762908d800f30cf04b8119
Nafees10 <nafeeshassan16@gmail.com>
2020-03-27 17:47:40 +0500

    sdfsdf

--- source/spinctrl/img/framegen.d
+++ source/spinctrl/img/framegen.d
@@ -13,28 +13,70 @@ import std.math;
 public alias Color = spinctrl.img.imgdef.Color;
 
 /// To generate RawFrames from PNG images or draw directly to them
-public class FrameMake{
-private:
-	ubyte _sectors; /// number of sectors
-public:
-	/// constructor
-	this(ubyte sectors){
-		_sectors = sectors;
-	}
-	~this(){
-
+public struct FrameMake{
+	/// stores angle+led for each pixel. Read as `_angleLed[(y * width)+x]`. [0] is angle, [1] is 
+	int[2][IMAGE_PIXELS_COUNT] _angleLed;
+	/// maps pixels onto _angleLed. Must be called before using any other function
+	void map(){
+		foreach (uint y; 0 .. IMAGE_LEN){
+			foreach (uint x; 0 .. IMAGE_LEN){
+				immutable index = (y*IMAGE_LEN) + x;
+				_angleLed[index] = getLedAngle(cast(ubyte)x, cast(ubyte)y);
+			}
+		}
 	}
 }
 
+/// calculaets [angle, led] for every pixel. Orders the result in ascending order by angle
+/// 
+/// Returns: [angle, led] for every pixel, sorted by angle (ascending)
+private int[2][] getLedAngle(){
+	int[2][IMAGE_PIXELS_COUNT] angleLeds;
+	foreach (uint y; 0 .. IMAGE_LEN){
+		foreach (uint x; 0 .. IMAGE_LEN){
+			immutable index = (y*IMAGE_LEN) + x;
+			angleLeds[index] = getLedAngle(cast(ubyte)x, cast(ubyte)y);
+		}
+	}
+	int[] angles;
+	angles.length = IMAGE_PIXELS_COUNT;
+	foreach (i, angleLed; angleLeds){
+		angles[i] = angleLed[0];
+	} 
+	immutable uinteger[] newIndexes = angles.sortAscendingIndex;
+	int[2][IMAGE_PIXELS_COUNT] orderedList;
+	foreach (i, originalIndex; newIndexes){
+		orderedList[i] = angleLeds[originalIndex];
+	}
+	uint discardCount = 0;
+	foreach (i; 0 .. IMAGE_PIXELS_COUNT){
+		if (orderedList[i][0] > -1)
+			break;
+		discardCount ++;
+	}
+	return orderedList[discardCount .. $].dup;
+}
+///
+unittest{
+	import std.conv : to;
+	int[2][] list = getLedAngle;
+	string[] strList;
+	strList.length = list.length;
+	foreach (i, val; list){
+		strList[i] = list[i].to!string;
+	}
+	arrayToFile(strList, "angles");
+}
+
 /// Returns: [angle, led] for a pixel at (x, y). If out of range, returns [-1, -1]
-int[2] getLedAngle(ubyte x, ubyte y){
+private int[2] getLedAngle(uint x, uint y){
 	/// center lines
-	immutable ubyte center = GROUP_COUNT*GROUP_LEDS_COUNT; // 20
+	immutable uint center = GROUP_COUNT*GROUP_LEDS_COUNT; // 20
 	/// stores quadrant
 	immutable ubyte quadrant = x < center ? (y <= center ? 2 : 3) : (y <= center ? 1 : 4);
 	// x, y, and hypotenuse distance from origin
-	immutable uint xDist = x == center ? 0 : abs(center - x);
-	immutable uint yDist = y == center ? 0 : abs(center - y);
+	immutable uint xDist = x == center ? 0 : abs(cast(int)center - cast(int)x);
+	immutable uint yDist = y == center ? 0 : abs(cast(int)center - cast(int)y);
 	immutable uint led = cast(immutable uint)round(sqrt(cast(float)(pow(xDist, 2) + pow(yDist, 2))));
 	if (xDist + yDist == 0 || led > center || led == 0)
 		return [-1, -1];
@@ -46,18 +88,16 @@ int[2] getLedAngle(ubyte x, ubyte y){
 }
 /// 
 unittest{
-	debug{
-		TrueColorImage img = new TrueColorImage(41, 41);
-		foreach (ubyte x; 0 .. 41){
-			foreach (ubyte y; 0 .. 41){
-				int[2] pos = getLedAngle(x, y);
-				if (pos[0] == -1 && pos[1] == -1){
-					img.setPixel(x, y, arsd.color.Color.red);
-				}else{
-					img.setPixel(x, y, arsd.color.Color.green);
-				}
+	TrueColorImage img = new TrueColorImage(IMAGE_LEN, IMAGE_LEN);
+	foreach (ubyte x; 0 .. IMAGE_LEN){
+		foreach (ubyte y; 0 .. IMAGE_LEN){
+			int[2] pos = getLedAngle(x, y);
+			if (pos[0] == -1 || pos[1] == -1){
+				img.setPixel(x, y, arsd.color.Color.red);
+			}else{
+				img.setPixel(x, y, arsd.color.Color.green);
 			}
 		}
-		writePng("spot.png", img);
 	}
+	writePng("spot.png", img);
 }
--- source/spinctrl/img/imgdef.d
+++ source/spinctrl/img/imgdef.d
@@ -8,8 +8,10 @@ import std.conv : to;
 const ubyte GROUP_LEDS_COUNT = 4;
 /// Number of groups of LEDs. safe to change, just be sure to put actual LEDs on spinner
 const ubyte GROUP_COUNT = 5;
-/// distance b/w LEDs
-const ubyte LED_DIST_MM = 5;
+/// Length of image
+const ubyte IMAGE_LEN = (2*GROUP_LEDS_COUNT*GROUP_COUNT) + 1;
+/// Number of pixels in a square image of length 2xGROUP_COUNT*GROUP_LEDS_COUNT
+const uint IMAGE_PIXELS_COUNT = IMAGE_LEN * IMAGE_LEN;
 
 /// possible status of LEDs
 package enum Color : ubyte{
@@ -19,86 +21,3 @@ package enum Color : ubyte{
 	Cyan = 0B00000011, /// .
 }
 
-/// To store a raw frame, uncompressed
-/// 
-/// `n` is the number of groups of LEDs available
-/// `_imgData.length` is the number of _imgData.length in a frame
-/// 
-/// keep in mind that the _imgData.length are read from LED closest to circumference at index 0
-package struct RawFrame{
-	/// constructor
-	this (ubyte sectorsCount){
-		this._imgData.length = sectorsCount;
-	}
-	/// Returns: number of sectors
-	@property ubyte sectors(){
-		return cast(ubyte)(_imgData.length);
-	}
-	/// stores the raw image, this directly represents the stream generated at end
-	ubyte[GROUP_COUNT][] _imgData;
-	/// postblit, makes sure all _imgData are separate
-	this(this){
-		_imgData = _imgData.dup;
-	}
-	/// Returns: status of LEDs at a sector.
-	Color[GROUP_COUNT*GROUP_LEDS_COUNT] readSector(ubyte sector){
-		Color[GROUP_COUNT * GROUP_LEDS_COUNT] r;
-		const ubyte[GROUP_COUNT] groups = _imgData[sector];
-		foreach(index, group; groups){
-			const ubyte index4 = cast(const ubyte)(index * 4);
-			foreach(i; 0 .. 4){
-				r[index4+i] = cast(Color)((group >>> (i*2)) % 4);
-			}
-		}
-		return r;
-	}
-	/// Writes to a sector
-	/// 
-	/// Returns: true on success, false on fail (i.e sector invalid)
-	bool writeSector(ubyte sector, Color[GROUP_COUNT*GROUP_LEDS_COUNT] ledStatus){
-		if (sector >= _imgData.length)
-			return false;
-		foreach(group; 0 .. GROUP_COUNT){
-			ubyte groupData;
-			const ubyte readIndex = cast(const ubyte)(group*GROUP_LEDS_COUNT);
-			foreach (i; 0 .. GROUP_LEDS_COUNT){
-				groupData += ledStatus[readIndex + i] << (i*2);
-			}
-			_imgData[sector][group] = groupData;
-		}
-		return true;
-	}
-	/// Converts this frame into a single stream of ubytes
-	ubyte[] toStream(){
-		ubyte[] r = [];
-		// start appending _imgData.length to it
-		uinteger writeIndex = 0;
-		r.length = _imgData.length * GROUP_COUNT;
-		foreach (sector; _imgData){
-			r[writeIndex .. writeIndex + GROUP_COUNT] = sector;
-			writeIndex += GROUP_COUNT;
-		}
-		return r;
-	}
-}
-/// 
-unittest{
-	import std.random : uniform;
-	auto frame = RawFrame(72);
-	Color[5 * GROUP_LEDS_COUNT][4] sequences;
-	// fill sequences randomly
-	foreach (i; 0 .. sequences.length){
-		foreach(led; 0 .. sequences[i].length){
-			sequences[i][led] = cast(Color)cast(ubyte)uniform(0, 4);
-		}
-	}
-	// write sequences to RawFrame
-	foreach (sector; 0 .. 72){
-		frame.writeSector(cast(ubyte)sector, cast(Color[20])sequences[sector % sequences.length]);
-	}
-	// now read and match
-	foreach (sector; 0 .. 72){
-		Color[20] read = frame.readSector(cast(ubyte)sector);
-		assert (read == cast(Color[20])sequences[sector % sequences.length]);
-	}
-}
